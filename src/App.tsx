f6fbb03de134c773fcd8f6c7a1509ad00b26ae1c
Luiz Fellipe Sousa <fellipe.skool@gmail.com>
2023-07-04 14:09:32 -0300

    Algumas mudanças no layout e a instalação do axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
+import Header from "./components/Header"
+
 function App() {
   return (
     <div className='h-full bg-zinc-900 text-zinc-300'>
-      <header className="flex justify-center bg-zinc-800 py-5">
-        <h1>Free Games Search</h1>
-      </header>
+      <Header />
 
       <div className="max-w-6xl my-8 mx-auto flex justify-center">
-        <main>
+        <main className="w-full">
           <form className="bg-zinc-800 p-2 rounded-lg flex items-center gap-8">
             <input className="rounded-lg w-full h-13 bg-zinc-900 p-4" type="text" placeholder="Nome do Jogo"/>
-            <button className="bg-blue-900 rounded-lg py-4 px-6" type="submit">Buscar</button>
+            <button className="bg-blue-500 hover:bg-blue-400 transition-colors duration-300 rounded-lg py-4 px-6 text-white" type="submit">Buscar</button>
           </form>
           <article className="bg-zinc-800 p-10 rounded-lg flex justify-center my-8">
             <p>GAME</p>
